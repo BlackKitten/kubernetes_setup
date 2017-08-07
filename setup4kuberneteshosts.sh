e68d5204262567579cd15189b4664eb126901648
Ben Bertrands <ben.bertrands@gmail.com>
2017-08-07 16:14:21 +0200

    added setup vagrant boxes command to setup4kuberneteshosts script

--- setup4kuberneteshosts.sh
+++ setup4kuberneteshosts.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+vagrant up &&
 for i in 2 3 4; do ssh vagrant@localhost -p 2222$i -i .vagrant/machines/kube$i/virtualbox/private_key -o "StrictHostKeyChecking no" < kubernetesSetup.txt; done
